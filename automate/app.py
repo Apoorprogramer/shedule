e3470a8a680b4f89daffad3816e5554df05b9fae
surya <86183551+Apoorprogramer@users.noreply.github.com>
2025-06-20 12:15:12 +0530

    Update app.py

--- automate/app.py
+++ automate/app.py
@@ -101,4 +101,4 @@ if __name__ == '__main__':
  if not os.path.exists(db_path):
   with app.app_context():
    db.create_all()
- app.run(host="0.0.0.0",port=int(os.environ.get('PORT',5000)))
+ app.run(host="0.0.0.0",port=int(os.environ.get('PORT',4000)))
